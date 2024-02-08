fed1d4c1e61e684605f2d6894746e5c44992bc06
Puhinsky <puhinsky@gmail.com>
2024-02-08 21:57:46 +0300

    Fix Drag Objects by Mouse

--- Assets/Scripts/UI/Views/MouseDraggable.cs
+++ Assets/Scripts/UI/Views/MouseDraggable.cs
@@ -7,21 +7,32 @@ namespace Puhinsky.DND.UI
     public class MouseDraggable : MonoBehaviour
     {
         private GameModel _gameModel;
+        private Vector3 _deltaPosition;
 
         private void Awake()
         {
             _gameModel = FindAnyObjectByType<GameStateMachine>().Model;
         }
 
+        private void OnMouseDown()
+        {
+            _deltaPosition = transform.position - GetMousePosition();
+        }
+
         private void OnMouseDrag()
         {
             if (_gameModel.State.Value == GameStateType.Game)
-                SetPosition(Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue()));
+                SetPosition(GetMousePosition() + _deltaPosition);
         }
 
         public void SetPosition(Vector3 rawPosition)
         {
             transform.position = new Vector3(rawPosition.x, rawPosition.y, transform.position.z);
+        }
+
+        private Vector3 GetMousePosition()
+        {
+            return Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
         }
     }
 }
